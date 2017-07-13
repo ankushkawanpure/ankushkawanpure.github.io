7178cd41c181b5aaf6f5d867426c013de29ec06f
ankush <ankushkawanpure@gmail.com>
2017-07-13 13:35:06 -0700

    back up

--- HTML/js/style-switcher.js
+++ HTML/js/style-switcher.js
@@ -33,44 +33,44 @@
 
 function createCookie(name,value,days)
 {
-	if (days)
-	{
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
+    if (days)
+    {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
 }
 function readCookie(name)
 {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++)
-	{
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++)
+    {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
 }
 function eraseCookie(name)
 {
-	createCookie(name,"",-1);
+    createCookie(name,"",-1);
 }
 
 // DEMO Swticher Base
 jQuery('.demo_changer .demo-icon').click(function(){
 
-	if(jQuery('.demo_changer').hasClass("active")){
-		jQuery('.demo_changer').animate({"left":"-113px"},function(){
-			jQuery('.demo_changer').toggleClass("active");
-		});						
-	}else{
-		jQuery('.demo_changer').animate({"left":"0px"},function(){
-			jQuery('.demo_changer').toggleClass("active");
-		});			
-	}
+    if(jQuery('.demo_changer').hasClass("active")){
+        jQuery('.demo_changer').animate({"left":"-113px"},function(){
+            jQuery('.demo_changer').toggleClass("active");
+        });
+    }else{
+        jQuery('.demo_changer').animate({"left":"0px"},function(){
+            jQuery('.demo_changer').toggleClass("active");
+        });
+    }
 
 });
 
